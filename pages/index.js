a54e3f54db5a07b19a620b39c1c6ca2209acddeb
Danila Legkobytov <bjorn-bis@yandex.ru>
2023-01-26 23:24:38 +0700

    bugfix: Добавить элемент

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+const headerSquareElement = document.querySelector(".header__square-pic");
+
 if (document.documentElement.clientWidth <= 896) {
   headerSquareElement.classList.remove("rotation");
 } else {
